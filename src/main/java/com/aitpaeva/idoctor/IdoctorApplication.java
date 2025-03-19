a93ee76c322b50faa61c2b4fc5674cad36c87616
aitpaeva00 <aizhan.aitpaeva@alatoo.edu.kg>
2025-03-19 16:55:55 +0600

    init commit

---
+++ src/main/java/com/aitpaeva/idoctor/IdoctorApplication.java
@@ -0,0 +1,13 @@
+package com.aitpaeva.idoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdoctorApplication.class, args);
+	}
+
+}
